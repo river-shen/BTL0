3848d991a07cc6833726235360b8e18d6ebbc755
Rivershen <tienlam3105@gmail.com>
2023-09-18 00:22:43 +0700

    fix

---
+++ BTL0/Constant/Constant.cs
@@ -0,0 +1,17 @@
+﻿namespace BTL0.Constant
+{
+    public abstract class StudentConstant
+    {
+        public const int MaxLengthName = 100;
+        public const int MaxLengthAddress = 300;
+        public const double MaxHeight = 300.0;
+        public const double MinHeight = 50.0;
+        public const double MaxWeight = 1000.0;
+        public const double MinWeight = 5.0;
+        public const int LengthStudentCode = 10;
+        public const int MaxLengthSchoolName = 200;
+        public const int MinYear = 1900;
+        public const double MinGpa = 0.0;
+        public const double MaxGpa = 10.0;
+    }
+}
